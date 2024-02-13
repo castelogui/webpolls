e1c045d59c1faf91fa407bb177e27bfe099f2c92
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-02-13 16:40:27 -0400

    fix: adjust function websocket and pollstate

--- composables/usePolls.js
+++ composables/usePolls.js
@@ -1,10 +1,15 @@
 export default () => {
   const sessionId = useCookie("sessionId");
-  let pollsList = [];
-  const api = "https://polls-xi-ten.vercel.app";
-  const apiDev = "http://localhost:3333";
-  const apiWsDev = "ws://localhost:3333";
-  const apiWs = "ws://polls-xi-ten.vercel.app";
+
+  //const apiWs_ = "ws://polls-xi-ten.vercel.app";
+  //const api_ = "https://polls-xi-ten.vercel.app";
+
+  const api = "http://localhost:3333";
+  const apiWs = "ws://localhost:3333";
+
+  const pollsList = () => useState("polls_list", () => []);
+  const pollsState = () => useState("polls_state", () => {});
+
   const fetchPollList = async () => {
     const { data } = await useAsyncData(
       "polls",
@@ -17,30 +22,35 @@ export default () => {
           },
         })
     );
+    let list = [];
     if (data.value) {
       data.value.map(async (poll) => {
-        pollsList.push(poll);
-        const ws = new WebSocket(`${apiWs}/polls/${poll.id}/results`, {
-          headers: {
-            "Content-Type": "application/json",
-          },
+        list.push(poll);
+        list.map(async (poll) => {
+          poll = await connectionWebSocket(poll);
         });
-        ws.onopen = () => {
-          console.log("Ws connection");
-        };
-        ws.onmessage = (message) => {
-          const pollOption = JSON.parse(message.data);
-          pollsList.map((poll) => {
-            poll.options.map((option) => {
-              if (option.id == pollOption.pollOptionId) {
-                option.score = pollOption.votes;
-              }
-            });
-          });
-        };
       });
     }
-    return pollsList;
+    list.map((poll) => {
+      pollsList().value = pollsList().value.filter((p) => p.id !== poll.id);
+      pollsList().value.push(poll);
+    });
+  };
+
+  const connectionWebSocket = async (poll) => {
+    const ws = new WebSocket(`${apiWs}/polls/${poll.id}/results`);
+    ws.onopen = () => {
+      console.log("Ws connection");
+    };
+    ws.onmessage = (message) => {
+      const pollOption = JSON.parse(message.data);
+      poll.options.map((option) => {
+        option.id == pollOption.pollOptionId
+          ? (option.score = pollOption.votes)
+          : false;
+      });
+    };
+    return poll;
   };
 
   const createPoll = async ({ title, options }) => {
@@ -66,7 +76,7 @@ export default () => {
   };
 
   const updatePoll = async (id, poll) => {
-    const { data } = await useAsyncData(
+    await useAsyncData(
       "poll",
       async () =>
         await $fetch(`${api}/polls/${id}`, {
@@ -84,46 +94,42 @@ export default () => {
   };
 
   const votePoll = async (pollId, pollOptionId) => {
-    const dataVote = await $fetch(`${api}/polls/${pollId}/votes`, {
-      method: "POST",
-      body: {
-        pollOptionId: pollOptionId,
-      },
+    try {
+      await $fetch(`${api}/polls/${pollId}/votes`, {
+        method: "POST",
+        body: {
+          pollOptionId: pollOptionId,
+        },
+        headers: {
+          getSetCookie: sessionId.value,
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deletePoll = async (id) => {
+    await $fetch(`${api}/polls/${id}`, {
+      method: "DELETE",
       headers: {
         getSetCookie: sessionId.value,
         "Content-Type": "application/json",
       },
     });
-    console.log(dataVote);
-  };
-
-  const deletePoll = async (id) => {
-    const { data, error, execute, pending, refresh, status } =
-      await useAsyncData(
-        "poll",
-        async () =>
-          await $fetch(`${api}/polls/${id}`, {
-            method: "DELETE",
-            headers: {
-              getSetCookie: sessionId.value,
-              "Content-Type": "application/json",
-            },
-          })
-      );
+    pollsList().value = pollsList().value.filter((poll) => poll.id !== id);
   };
 
   const getPoll = async (id) => {
     try {
       const data = await $fetch(`${api}/polls/${id}`, {
         method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
       });
-      return data.poll;
+      pollsState().value = await connectionWebSocket(data.poll);
+      return pollsState().value;
     } catch (error) {
       console.log(error);
-      return false;
     }
   };
 
@@ -135,5 +141,6 @@ export default () => {
     getPoll,
     votePoll,
     pollsList,
+    pollsState,
   };
 };
